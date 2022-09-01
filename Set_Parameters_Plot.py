22819557bb66b176ef719cd3cf6d12f0186062b9
ThaisAbelha <abelhaemail@gmail.com>
2022-09-01 19:17:43 -0300

    small changes

--- Set_Parameters_Plot.py
+++ Set_Parameters_Plot.py
@@ -1,4 +1,3 @@
-#%%
 '''
 Author: Thais Silva Abelha
 Email: thais.silva.abelha@gmail.com
@@ -17,6 +16,7 @@ import pandas as pd
 WAC = weighted average considering the charge as a weight
 WACC = weighted average considering the charge*charge as a weighet
 WAlnC = weighted average considering the ln(charge) as a weight
+Q = charge
 s=standard deviation or sigma σ of the gaussian
 u=mean of the gaussian or electron cloud center
 
@@ -24,25 +24,54 @@ prefix "s" stands for standard deviation or sigma σ
 
 RMS = Root Mean Square 
 '''
-
-def Dataframe(strip_width,pitch,slist,signoiselist,Position_strips):
+def Plot_Cluster(first_evt_cluster,u,s,Labels):
+    fig, ax = plt.subplots()
+    ax.plot(first_evt_cluster['Position_strip'],first_evt_cluster['Charge_strip_noise'],"o", markersize=4,label=(Labels["Label_Charges"]))
+    ax.plot(first_evt_cluster['Position_Cluster'],first_evt_cluster['Charge_Cluster'],"*",markersize=7,label=Labels["Label_Cluster"])
+    fig.suptitle(Labels["Title"])
+    ax.set_xlabel(Labels["xlabel"])
+    ax.set_ylabel(Labels["ylabel"])
+    
+    ax.legend(loc='upper right')
+    ax.grid()
+    ax.set_ylim(-0.05,0.35)
+    
+    textstr = '\n'.join((
+    r'$\mu=%.2f$' % (u, )+"mm",
+    r'$\sigma=%.2f$' % (s, )+"mm"
+    ))
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    ax.text(0.08, 0.95, textstr, transform=ax.transAxes, fontsize=11,verticalalignment='top', bbox=props)
+    
+    plt.show()
+    
+def Dataframe(strip_width,pitch,slist,signoiselist,Position_strips,electron_cloud_centers):
     '''For the case when    pitch=0.39mm, the electron cloud center is going to be reconstructed approximately -0.8mm<x<0.8mm
     Also the electron cloud center is going to be reconstructed with steps 10 times smaller than the strip pitch'''
-    #u0=-2*pitch  #First position of the electron Cloud relative to the readout strips (u = mean value of the gaussian)
-    u0=-2*pitch
-    du=pitch/10  #Step for the electron cloud positions
     
+    #You can comment "Plot_Cluster()" It is just a Graph of control then it is going to run faster
+    #Plot_Cluster() is used just to see if verything is fine with the algorithm
+    
+    Labels={"Title":"Cluster - Event 1/1000",
+            "xlabel":"Position of the strip center (mm)",
+            "ylabel":"Charge collected by each strip (mm)",
+            "Label_Charges":"Charge collected by strips",
+            "Label_Cluster":"Cluster"}
+    
+    Labels={"Title":"Cluster - Evento 1/1000",
+            "xlabel":"Posição do centro da fita (mm)",
+            "ylabel":"Carga coletada por cada fita",
+            "Label_Charges":"Carga coletada pelas fitas",
+            "Label_Cluster":"Cluster"}
     Data=[]
     for s in slist:
         sig_noise=snoiselist[slist.index(s)]
-        u=u0
-        while u<2*pitch:
-            Charge,sCharge,E_WAC,sE_WAC,E_WACC,sE_WACC,E_WAlnC,sE_WAlnC=Cloud_Collection_Simulation(strip_width,pitch, s, u,sig_noise,Position_strips).Monte_Carlo()
+        for u in electron_cloud_centers:
+            Charge,sCharge,E_WAC,sE_WAC,E_WACC,sE_WACC,E_WAlnC,sE_WAlnC,first_evt_cluster=Cloud_Collection_Simulation(strip_width,pitch, s, u,sig_noise,Position_strips).Monte_Carlo()
             Data.append([s,u,Charge,sCharge,E_WAC,sE_WAC,E_WACC,sE_WACC,E_WAlnC,sE_WAlnC])
-            u=u+du
+            Plot_Cluster(first_evt_cluster,u,s,Labels)
     df = pd.DataFrame(Data, columns=["s","x","Q","sQ","E_WAC","sE_WAC","E_WACC","sE_WACC","E_WAlnC","sE_WAlnC"])
     return df
-
     
 def Plot_Charge_Error(Name,slist, df, markers, markersizes,ymin,ymax,Title,Ylabel):
     for s in slist:
@@ -61,36 +90,50 @@ def Plot_Charge_Error(Name,slist, df, markers, markersizes,ymin,ymax,Title,Ylabe
     plt.grid(True)
     plt.show()
     
-def rms(v):
-    return np.sqrt((v*v).sum()/v.shape[0])
+def RMS(v):
+    return np.sqrt((v*v).sum()/len(v))
 
-def rms_uncertainty_propagation(v,sv):
-    return np.sqrt(sum((v*sv)**2))/(len(v)*rms(v))
+def RMS_uncertainty_propagation(v,sv):
+    return np.sqrt(sum((v*sv)**2))/(len(v)*RMS(v))
 
-def rms_Dataframe(slist,df):
+def RMS_Dataframe(slist,df):
     Data=[]
     for s in slist:
-        Data.append([s,
-                     rms(df.loc[df['s'].isin([s]),'E_WAC']), 
-                     rms_uncertainty_propagation(df.loc[df['s'].isin([s]),'E_WAC'],df.loc[df['s'].isin([s]),'sE_WAC']), 
-                     rms(df.loc[df['s'].isin([s]),'E_WACC']), 
-                     rms_uncertainty_propagation(df.loc[df['s'].isin([s]),'E_WACC'],df.loc[df['s'].isin([s]),'sE_WACC']), 
-                     rms(df.loc[df['s'].isin([s]),'E_WAlnC']), 
-                     rms_uncertainty_propagation(df.loc[df['s'].isin([s]),'E_WAlnC'],df.loc[df['s'].isin([s]),'sE_WAlnC'])
+        Data.append([
+                     s,
+                     RMS(df.loc[df['s'].isin([s]),'E_WAC']), 
+                     RMS_uncertainty_propagation(df.loc[df['s'].isin([s]),'E_WAC'],df.loc[df['s'].isin([s]),'sE_WAC']), 
+                     RMS(df.loc[df['s'].isin([s]),'E_WACC']), 
+                     RMS_uncertainty_propagation(df.loc[df['s'].isin([s]),'E_WACC'],df.loc[df['s'].isin([s]),'sE_WACC']), 
+                     RMS(df.loc[df['s'].isin([s]),'E_WAlnC']), 
+                     RMS_uncertainty_propagation(df.loc[df['s'].isin([s]),'E_WAlnC'],df.loc[df['s'].isin([s]),'sE_WAlnC'])
                      ])
         
-    rms_df = pd.DataFrame(Data,columns=['s','E_WAC','sE_WAC','E_WACC','sE_WACC','E_WAlnC','sE_WAlnC'])
-    return rms_df
+    RMS_df = pd.DataFrame(Data,columns=['s','E_WAC','sE_WAC','E_WACC','sE_WACC','E_WAlnC','sE_WAlnC'])
+    return RMS_df
 
-'''
-for this study we have modeled a 10mm readout, from -5mm to 5mm  where the coordinate 0 is the middle of the
+def Plot_RMS(RMS_df,Labels):
+    plt.title(Labels["Title"]) 
+    plt.errorbar(RMS_df['s'],RMS_df['E_WAC'],marker=markers[0], markersize=markersizes[0]*1.5,linestyle='' ,label="Linear weight",xerr=0, yerr=RMS_df['sE_WAC'])
+    plt.errorbar(RMS_df['s'],RMS_df['E_WACC'],marker=markers[1],  markersize=markersizes[1]*1.5, linestyle='' ,label="Squared weight",xerr=0, yerr=RMS_df['sE_WACC'])
+    plt.errorbar(RMS_df['s'],RMS_df['E_WAlnC'],marker=markers[2],  markersize=markersizes[2]*1.5,linestyle='' ,label="Logarithmic weight",xerr=0, yerr=RMS_df['sE_WAlnC'])
+    plt.ylabel(Labels["ylabel"]) 
+    
+    ax = plt.gca()
+    handles, labels = ax.get_legend_handles_labels()
+    handles = [h[0] if isinstance(h, container.ErrorbarContainer) else h for h in handles]
+    ax.legend(handles, labels,loc='upper right')
+    plt.xlabel(r'$\sigma$ (mm)')  
+    plt.grid(True)
+    plt.show()
+
+#%%
+'''For this study we have modeled a 10mm readout, from -5mm to 5mm  where the coordinate 0 is the middle of the
 strips pattern, in this case. The clouds were generated in the range −0.8mm to 0.8mm, so the cloud didn’t
 suffer edge effect. The charge clouds were modeled as normalized Gaussian functions centered
-in μ with standard deviation σ
-
-'''
+in μ with standard deviation σ'''
 ###############################################################################
-#Input
+#Chose the parameters for the 1D readout geometry
 ###############################################################################
 strip_width=0.2 #The typical strip width is 0.2mm
 #The typical strip pitch is 0.39mm (100mm/256strips)
@@ -101,47 +144,44 @@ slist=[0.2,0.25,0.3,0.35,0.4]
 snoiselist=[0.01,0.01,0.01,0.01,0.01]
 ###############################################################################
 #Calculating the Collected Charge and the Errors of Position Reconstruction
-###############################################################################
 #Region we are going to calculate the charge collected by the strips and strip width
-first_strip_pos=-5
-last_strip_pos=5
+###############################################################################
+first_strip_pos=-5#(mm)
+last_strip_pos=5#(mm)
+first_cloud_pos=-2*pitch#(mm)
+last_cloud_pos=2*pitch#(mm)
 
-#Defining the strip positions
-Position_strips=[]
-p=first_strip_pos
-i=0
-while p<(last_strip_pos):
-    Position_strips.append(p)
-    p=p+pitch
-    i=i+1
+###############################################################################
+#Defining the strip certer positions
+#And positions of the electron cloud centers
+###############################################################################
+strip_centers=np.arange(first_strip_pos,last_strip_pos,pitch)
+electron_cloud_centers=np.arange(first_cloud_pos,last_cloud_pos,pitch/10)
     
-'''Now we are just organazing the data in a Matrix to save run time. We are calculating the data for the colected charge and 
+'''Now we are just organazing the data in a DataFrame. We are calculating the data for the colected charge and 
 the Errors whith linear, quadratic and logarithmic weights, considering different sigmas for the gaussian that 
 stands for the electron cloud'''
-###############################################################################
-#Matrix with the data
-###############################################################################
-#x,Charges_Matrix,sCharges_Matrix,E_WAC_Matrix,sE_WAC_Matrix,E_WACC_Matrix,sE_WACC_Matrix,E_WAlnC_Matrix,sE_WAlnC_Matrix=Charge_ErrorPosition_Matrix(strip_width,pitch,slist,snoiselist,Position_strips)
-###############################################################################
-df=Dataframe(strip_width,pitch,slist,snoiselist,Position_strips)
-#%%
-#y axis limits
-ymax=0.6
-ymin=0.45
-s_indexes_toplot=[0,2,4] #List with the indexes of the cloud widths (sigmas) we are going to plot
 
+df=Dataframe(strip_width,pitch,slist,snoiselist,strip_centers,electron_cloud_centers)
+#%%############################################################################
+#Graphs that depend on electron cloud position relative to the readout strips
+###############################################################################
+#List with the indexes of the cloud widths (sigmas or standar deviations) we are going to plot in the Graph of the Errors
+s_indexes_toplot=[0,2,4] 
 markers=['s','^','P','*','x']
 markersizes=[4,4,4,14/3,4]
+ymax=0.6
+ymin=0.45
 
 Title={"Q":"Collected charge",
        "E_WAC":"Position reconstruction using a linear weight",
-          "E_WACC":"Position reconstruction using a squared weight",
-          "E_WAlnC": "Position reconstruction using a logarithmic weight"}
-Titulo={"Q":'Carga coletada pelas fitas',
-        "E_WAC":"Reconstrução da posição usando peso linear",
+       "E_WACC":"Position reconstruction using a squared weight",
+       "E_WAlnC": "Position reconstruction using a logarithmic weight"}
+
+Title_pt={"Q":'Carga coletada pelas fitas',
+          "E_WAC":"Reconstrução da posição usando peso linear",
           "E_WACC":"Reconstrução da posição usando peso quadrático",
           "E_WAlnC": "Reconstrução da posição usando peso logaritmico"}
-
 
 Ylabel={"Q":'Fraction of collected charge',"E":'Error (mm)'}
 Ylabel_pt={"Q":'Fração de carga coletada',"E":'Erro (mm)'}
@@ -157,29 +197,15 @@ Plot_Charge_Error("E_WAC",slist, df, markers, markersizes,ymin,ymax,Title["E_WAC
 Plot_Charge_Error("E_WACC",slist, df, markers, markersizes,ymin,ymax,Title["E_WACC"],Ylabel['E'])
 Plot_Charge_Error("E_WAlnC",slist, df, markers, markersizes,ymin,ymax,Title["E_WAlnC"],Ylabel['E'])
 
-rms_df=rms_Dataframe(slist,df)
+#%%############################################################################
+#RMS Graph that depend on the electron cloud width (standard deviation of the gaussian)
+###############################################################################
+RMS_df=RMS_Dataframe(slist,df)
 
-#RMS Graph
-#En
-plt.title("Comparing methods for position reconstruction") 
-plt.errorbar(rms_df['s'],rms_df['E_WAC'],marker=markers[0], markersize=markersizes[0]*1.5,linestyle='' ,label="Linear weight",xerr=0, yerr=rms_df['sE_WAC'])
-plt.errorbar(rms_df['s'],rms_df['E_WACC'],marker=markers[1],  markersize=markersizes[1]*1.5, linestyle='' ,label="Squared weight",xerr=0, yerr=rms_df['sE_WACC'])
-plt.errorbar(rms_df['s'],rms_df['E_WAlnC'],marker=markers[2],  markersize=markersizes[2]*1.5,linestyle='' ,label="Logarithmic weight",xerr=0, yerr=rms_df['sE_WAlnC'])
-plt.ylabel('Error RMS (mm)') #En
+Labels={"Title":"Comparing methods for position reconstruction",
+        "ylabel":'RMS of the Error (mm)'}
 
-'''
-#Pt
-plt.title("Comparando diferente métodos para reconstrução da posição") #Pt
-plt.errorbar(slist,RMS_WAC, marker=markers[0], markersize=markersizes[0]*1.5, linestyle='' , label="Peso linear", xerr=0, yerr=sRMS_WAC)
-plt.errorbar(slist,RMS_WACC, marker=markers[1], markersize=markersizes[1]*1.5, linestyle='' , label="Peso quadrático", xerr=0, yerr=sRMS_WACC)
-plt.errorbar(slist,RMS_WAlnC, marker=markers[2], markersize=markersizes[2]*1.5, linestyle='' , label="Peso logarítmico", xerr=0, yerr=sRMS_WAlnC)
-plt.ylabel('RMS do Erro (mm)') 
-'''
+Labels_pt={"Title":"Comparing methods for position reconstruction",
+           "ylabel":'Erro do RMS (mm)'}
 
-ax = plt.gca()
-handles, labels = ax.get_legend_handles_labels()
-handles = [h[0] if isinstance(h, container.ErrorbarContainer) else h for h in handles]
-ax.legend(handles, labels,loc='upper right')
-plt.xlabel(r'$\sigma$ (mm)')  
-plt.grid(True)
-plt.show()
+Plot_RMS(RMS_df,Labels)
--- cloud_collection_simulation.py
+++ cloud_collection_simulation.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.integrate import quad
 from cluster import Cluster
 from errors import Errors
+import pandas as pd
 
 ''''
 This simulates all electron clouds and the noise.
@@ -30,91 +31,42 @@ class Cloud_Collection_Simulation:
     def IntegraStrip(self,p):
         I = quad(self.Gauss, p-self.strip_width/2, p+self.strip_width/2, args=(self.s,self.u))
         return I[0]
-    
+
     def Charge_StripPos(self):
         Charges_strips=[self.IntegraStrip(p) for p in self.Position_strips]
         return Charges_strips,self.Position_strips
     
     def Monte_Carlo(self):
-        #Collected Charge
-        List_TotalCharge=np.zeros(self.N)
-        List_TotalCharge_Cluster=np.zeros(self.N)
-        #Position reconstruction Errors
-        E_WAC_List=np.zeros(self.N)
-        E_WACC_List=np.zeros(self.N)
-        E_WAlnC_List=np.zeros(self.N)
-               
+        Data=[]                           
         Charge_strip,Position_strip=self.Charge_StripPos()
         
         for i in range(self.N):
             Noise=np.random.normal(0,self.sig,len(Charge_strip))
             
             Charge_strip_noise=Charge_strip+Noise
-            Sum_Charge=sum(Charge_strip_noise)
-            List_TotalCharge[i]=Sum_Charge
-        
             Charge_Cluster,Position_Cluster=Cluster(self.seed,Charge_strip_noise,Position_strip).Find_Cluster()
-            
-            '''
-            Here we plot the clusterization of the first event, for each electron width.
-            It is done to have a graph of control, to see if everything works as it is
-            supposed to work.
-            '''
-            
+            Data.append([sum(Charge_Cluster),Errors(self.u, Charge_Cluster,Position_Cluster).E_WAC(),Errors(self.u, Charge_Cluster,Position_Cluster).E_WACC(),Errors(self.u, Charge_Cluster,Position_Cluster).E_WAlnC()])
+
             if i==0:
-                '''fig, ax = plt.subplots()'''
-
-                #En
-                '''ax.plot(Position_strip,Charge_strip_noise,"o", markersize=4,label="Charge collected by strips")
-                ax.plot(Position_Cluster,Charge_Cluster,"*",markersize=7,label="Cluster")
-                ax.legend(loc='upper right')
-                ax.title('Charge cloud centered on '+r'$\mu$='+"%2.2f"%(self.u)+' mm position, '+r'$\sigma$ '+str(self.s)+" mm")#+ r' $\mu$' +"%2.2f"%(self.u)+" mm e ")
-                ax.xlabel("Position of the strip center (mm)")
-                ax.ylabel("Charge collected by each strip (mm)")'''
+                first_evt_cluster={"Position_strip":Position_strip,
+                                   "Charge_strip_noise":Charge_strip_noise,
+                                   "Position_Cluster":Position_Cluster,
+                                   "Charge_Cluster":Charge_Cluster }
                 
-                #Pt
-                '''
-                ax.plot(Position_strip,Charge_strip_noise,"o", markersize=4,label="Carga coletada pelas strips")
-                ax.plot(Position_Cluster,Charge_Cluster,"*",markersize=7,label="Cluster (nuvem de elétrons)")
-                ax.legend(loc='upper right')
-                fig.suptitle("Clusterização")
-                ax.set_xlabel("Posição do centro da fita (mm)")
-                ax.set_ylabel("Carga coletada por cada fita (mm)")
-                ax.grid()
-                ax.set_ylim(-0.05,0.35)
-                
-                textstr = '\n'.join((
-                r'$\mu=%.2f$' % (self.u, )+"mm",
-                r'$\sigma=%.2f$' % (self.s, )+"mm"
-                ))
-                props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-                ax.text(0.08, 0.95, textstr, transform=ax.transAxes, fontsize=11,verticalalignment='top', bbox=props)
-
-                plt.show()
-                '''
-            somaCharge_Cluster=sum(Charge_Cluster)
-            List_TotalCharge_Cluster[i]=somaCharge_Cluster
-                    
-            Charge_Cluster=np.asarray(Charge_Cluster)
-            Position_Cluster=np.asarray(Position_Cluster)
-            
-            E_WAC_List[i]=Errors(self.u, Charge_Cluster,Position_Cluster).E_WAC()
-            E_WACC_List[i]=Errors(self.u, Charge_Cluster,Position_Cluster).E_WACC()
-            E_WAlnC_List[i]=Errors(self.u, Charge_Cluster,Position_Cluster).E_WAlnC()
-
-        Charge_mean=np.mean(List_TotalCharge_Cluster)
-        sCharge=np.std(List_TotalCharge_Cluster,ddof=1)/np.sqrt(self.N)
+        df=pd.DataFrame(Data, columns=["Q","E_WAC","E_WACC","E_WAlnC"])
         
         '''
-        Errors considering the three different methods
+        Considering N events we are going to calaculate Error means and Stardard deviations of the Errors menas
         '''
         
-        E_MPmean_C=np.mean(E_WAC_List)
-        E_MPmean_CC=np.mean(E_WACC_List)
-        E_MPmean_lnC=np.mean(E_WAlnC_List)
+        Charge_mean=np.mean(df['Q'])
+        E_WAC=np.mean(df['E_WAC'])
+        E_WACC=np.mean(df['E_WACC'])
+        E_WAlnC=np.mean(df['E_WAlnC'])
         
-        E_MPs_C=np.std(E_WAC_List,ddof=1)/np.sqrt(self.N)
-        E_MPs_CC=np.std(E_WACC_List,ddof=1)/np.sqrt(self.N)   
-        E_MPs_lnC=np.std(E_WAlnC_List,ddof=1)/np.sqrt(self.N)
+        sCharge=np.std(df['Q'],ddof=1)/np.sqrt(self.N)
+        sE_WAC=np.std(df['E_WAC'],ddof=1)/np.sqrt(self.N)
+        sE_WACC=np.std(df['E_WACC'],ddof=1)/np.sqrt(self.N)   
+        sE_WAlnC=np.std(df['E_WAlnC'],ddof=1)/np.sqrt(self.N)
         
-        return Charge_mean,sCharge,E_MPmean_C,E_MPs_C,E_MPmean_CC,E_MPs_CC,E_MPmean_lnC,E_MPs_lnC
+        return Charge_mean,sCharge,E_WAC,sE_WAC,E_WACC,sE_WACC,E_WAlnC,sE_WAlnC,first_evt_cluster
